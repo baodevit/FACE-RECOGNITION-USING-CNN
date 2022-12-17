396317f019cd5eb8a910ceba7e51b09365b12062
baodev <baodev.it@gmail.com>
2022-12-17 14:00:21 +0700

    update 1

--- train.py
+++ train.py
@@ -9,4 +9,13 @@ def training_model():
     names, images = utils.load_datasets(dataset=datasets)
     print("Dataset: {} sample / {} labels".format(len(names), len(np.unique(names))))
     print("List labels: {}".format(np.unique(names)))
+
+    label_name_vector, labels = utils.convert_categorical(names)
+
+    x_train, x_test, y_train, y_test = utils.split_dataset(images=images, label_name_vector=label_name_vector, test_size=0.2)
+    print("x_train: {}".format(x_train.shape[0]))
+    print("x_test: {}".format(x_test.shape[0]))
+    print("y_train: {}".format(y_train.shape[0]))
+    print("y_test: {}".format(y_test.shape[0]))
+
 training_model()
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import os
 import cv2
+import numpy as np
 from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 import matplotlib as plt
 
 
@@ -36,6 +38,14 @@ def convert_categorical(names):
     name_vector = le.transform(names)
     label_name_vector = to_categorical(name_vector)
     return label_name_vector, labels
+
+
+def split_dataset(images, label_name_vector, test_size=0.2):
+    x_train, x_test, y_train, y_test = train_test_split(np.array(images, dtype=np.float32),
+                                                        np.array(label_name_vector),
+                                                        test_size=test_size,
+                                                        random_state=42)
+    return x_train, x_test, y_train, y_test
 
 
 def detector_face():
